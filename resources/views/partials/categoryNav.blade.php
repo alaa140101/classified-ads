00d49566561e872997c10af404b75730173ccc80
alaa140101 <alaa140011@gmail.com>
2022-03-08 06:54:59 +0300

    main template

---
+++ resources/views/partials/categoryNav.blade.php
@@ -0,0 +1,10 @@
+<ul class="nav justify-content-center">
+    {{-- @foreach($categories as $category)
+        <li class="nav-item">
+            <a class="nav-link active" href="/category/{{$category->id}}/{{$category->slug}}">{{$category->category_name}}</a>
+        </li>
+    @endforeach --}}
+        <li class="nav-item">
+            <a class="nav-link active" href="#">drhehr</a>
+        </li>
+</ul>
